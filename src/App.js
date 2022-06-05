bb6bd2d978ca8871789ea95242cc65a2b058d434
Vishal Sharma <sharma.vishal2799@gmail.com>
2022-06-05 12:18:30 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-import { Login } from "./components";
-import Home from "./container/Home";
+import { Login } from './components';
+import Home from './container/Home';
 
 const App = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    // eslint-disable-next-line operator-linebreak
     const User =
-      localStorage.getItem("user") !== "undefined"
-        ? JSON.parse(localStorage.getItem("user"))
+      localStorage.getItem('user') !== 'undefined'
+        ? JSON.parse(localStorage.getItem('user'))
         : localStorage.clear();
 
-    if (!User) navigate("/login");
+    if (!User) navigate('/login');
   }, []);
 
   return (
     <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
       <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="/*" element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='/*' element={<Home />} />
       </Routes>
     </GoogleOAuthProvider>
   );
